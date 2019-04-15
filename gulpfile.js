0d7ceaef16889b078e5c4d8fe35f1ce7a3683ad0
wsq <649704398@qq.com>
2019-04-15 09:58:46 +0800

    页面完成

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const mincss = require("gulp-clean-css");
 const server = require("gulp-webserver");
 const concat = require("gulp-concat");
 const connect = require("gulp-connect");
+const fs = require("fs");
+const url = require("url");
+const path = require("path");
 const proxy = require("http-proxy-middleware");
 
 gulp.task("css", () => {
@@ -17,7 +20,7 @@ gulp.task("js", () => {
     return gulp.src("./src/js/*.js")
         .pipe(concat("main.min.js"))
         .pipe(minjs())
-        .pipe(gulp.dest("./dist/css"))
+        .pipe(gulp.dest("./dist/js"))
 })
 gulp.task("webser", () => {
     return gulp.src("./")
@@ -26,6 +29,17 @@ gulp.task("webser", () => {
             port: "8080",
             open: true,
             livereload: true,
-            directoryListing: true
+            directoryListing: true,
+            middleware: (req, res) => {
+                var paths = url.parse(req.url).pathname;
+                if (req.url == "/") {
+                    var view = fs.readFileSync(path.join(__dirname, "src", "index.html"))
+                    res.end(view);
+                } else {
+                    var view = fs.readFileSync(path.join(__dirname, "src", paths))
+                    res.end(view);
+                }
+
+            }
         }))
 })
